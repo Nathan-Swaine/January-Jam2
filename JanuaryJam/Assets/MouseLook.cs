b39d611fa00b23eefc85bc84e5d5bc2fae6ccf89
Nathan <nathan.swaine@parall.ax>
2021-01-05 09:51:02 +0000

    more mouselook scripting

--- JanuaryJam/Assets/MouseLook.cs
+++ JanuaryJam/Assets/MouseLook.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float MouseSensitivity = 200f;
+    public float MouseSensitivity = 100f;
     public Transform PlayerBody; 
     float xRoataion = 0f;
     
     void Start() // Start is called before the first frame update
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update() // Update is called once per frame
     {
         float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime; 
         float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
         xRoataion -= mouseY;
-        xRoataion = Mathf.Clamp(xRoataion, -90f, 90f);
-        transform.localRotation = Quaternion.Euler (xRoataion, 0f, 0f);
-        PlayerBody.Rotate(Vector3.up * mouseX); 
+        xRoataion = Mathf.Clamp(xRoataion, -85f, 85f);
+        transform.localRotation = Quaternion.Euler(xRoataion, 0f, 0f);
+        //PlayerBody.Rotate(Vector2.up * mouseX); 
+        PlayerBody.Rotate(Vector3.up *mouseX);
     }
 }
