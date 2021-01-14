778402b8a3162b5b4c6a9c868f253e0072a5ed3d
DustinHoward1225 <49665331+DustinHoward1225@users.noreply.github.com>
2021-01-14 00:03:22 -0500

    project check
    meant to do some level stuff but didn't want to screw up anything...

--- JanuaryJam/Assets/Wait.cs
+++ JanuaryJam/Assets/Wait.cs
@@ -11,20 +11,20 @@ public class Wait : MonoBehaviour
     }
 
     IEnumerator Waits()
-{
-    //Rotate 90 deg
-    transform.Rotate(new Vector3(90, 0, 0), Space.World);
+    {
+        //Rotate 90 deg
+        transform.Rotate(new Vector3(90, 0, 0), Space.World);
 
-    //Wait for 4 seconds
-    yield return new WaitForSeconds(4);
+        //Wait for 4 seconds
+        yield return new WaitForSeconds(4);
 
-    //Rotate 40 deg
-    transform.Rotate(new Vector3(40, 0, 0), Space.World);
+        //Rotate 40 deg
+        transform.Rotate(new Vector3(40, 0, 0), Space.World);
 
-    //Wait for 2 seconds
-    yield return new WaitForSeconds(2);
+        //Wait for 2 seconds
+        yield return new WaitForSeconds(2);
 
-    //Rotate 20 deg
-    transform.Rotate(new Vector3(20, 0, 0), Space.World);
-}
+        //Rotate 20 deg
+        transform.Rotate(new Vector3(20, 0, 0), Space.World);
+    }
 }
